370a4463c136a87189164f2ba8009b2eefb755f2
马海龙 <630078606@qq.com>
2017-07-26 08:43:46 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,76 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage, IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
+import { AboutPage } from '../pages/about/about';
+import { ArticlePage } from '../pages/article/article';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { MessagePage } from '../pages/message/message';
+import { PublishPage } from '../pages/publish/publish';
+import { UserPage } from '../pages/user/user';
+
+import { Api } from '../providers/api';
+import { Items } from '../mocks/providers/items';
+import { Settings } from '../providers/settings';
+import { User } from '../providers/user';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+export function provideSettings(storage: Storage) {
+  /**
+   * The Settings provider takes a set of default settings for your app.
+   *
+   * You can add new settings options at any time. Once the settings are saved,
+   * these values will not overwrite the saved values (this can be done manually if desired).
+   */
+  return new Settings(storage, {
+    option1: true,
+    option2: 'Ionitron J. Framework',
+    option3: '3',
+    option4: 'Hello'
+  });
+}
+
 @NgModule({
   declarations: [
     MyApp,
+    AboutPage,
+    ArticlePage,
     HomePage,
-    ListPage
+    LoginPage,
+    MessagePage,
+    PublishPage,
+    UserPage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    AboutPage,
+    ArticlePage,
     HomePage,
-    ListPage
+    LoginPage,
+    MessagePage,
+    PublishPage,
+    UserPage
   ],
   providers: [
+    Api,
+    Items,
+    User,
     StatusBar,
     SplashScreen,
+    { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
